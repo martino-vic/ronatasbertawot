7b8e8eafc81b07c5f48a9ca75ad655ba2576a497
martino-vic <viktor.martinovic@hotmail.com>
2022-03-30 15:54:17 +0000

    added soundchanges.txt

--- raw/misc/makesc.py
+++ raw/misc/makesc.py
@@ -4,15 +4,17 @@ from pathlib import Path
 from loanpy import qfysc
 
 def main():
-    """creates othography.tsv with Graphemes 2 IPA mappings"""
+    """creates soundchanges.txt for horizontal or vertical transfers"""
 
-    #create orthography.py
+    #define in and out paths
     in_path1 = Path.cwd().parent.parent / "cldf" / "forms.csv"
     in_path2 = Path.cwd().parent / "wot.tsv"
-    out_path = Path.cwd().parent.parent / "etc" / "sc_vertical.tsv"
+    out_path = Path.cwd().parent.parent / "etc" / "soundchanges"
 
-    Test = qfysc.Qfy(dfetymology=(in_path1, in_path2, "EAH", "H"), left="Target_Form", right="Source_Form")
-    Test.dfetymology2dict(write=True)
+    #run qfysc module from loanpy
+    Etym = qfysc.Qfy(dfetymology=(in_path1, in_path2, "EAH", "H"),
+                     left="Target_Form", right="Source_Form", vfb=None)
+    Etym.dfetymology2dict(write=True, outname=out_path)
 
 if __name__ == "__main__":
     main()
